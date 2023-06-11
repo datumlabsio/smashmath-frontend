0f08ee3d8962a12c6b60abbd38e01666bc84ebc2
Bilal Saleem <bilalsaleem.dev@gmail.com>
2023-06-11 18:08:44 +0500

    remove console

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import AppRoutes from "./components/routes/AppRoutes";
 import { Toaster } from "react-hot-toast";
 const App = () => {
+
+  function noop() { }
+  (() => {
+    // if (process.env.NODE_ENV !== 'development') {
+      console.log = noop;
+      console.warn = noop;
+      console.error = noop;
+    // }
+  })();
+
   return (
     <>
       <Toaster />
